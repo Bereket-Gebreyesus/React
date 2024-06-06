88dadd7c3e776851f8056f1122350a2adc48aa4d
Be-Ge <bereketgebreyesusnl@gmail.com>
2024-06-06 11:29:32 +0200

    cleaning code

--- week1/project/ecommerce/src/components/CategoryList.jsx
+++ week1/project/ecommerce/src/components/CategoryList.jsx
@@ -1,23 +1,3 @@
-// import React from 'react';
-
-// function CategoryList({ categories, selectedCategory, onCategorySelect }) {
-//   return (
-//     <div className="category-list">
-//       {categories.map(category => (
-//         <button 
-//           key={category} 
-//           onClick={() => onCategorySelect(category)}
-//           className={selectedCategory === category ? 'active' : ''}
-//         >
-//           {category}
-//         </button>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default CategoryList;
-
 import React from 'react';
 
 const CategoryList = ({ categories, selectedCategory, onSelectCategory }) => {
--- week1/project/ecommerce/src/components/ProductList.jsx
+++ week1/project/ecommerce/src/components/ProductList.jsx
@@ -1,23 +1,3 @@
-// import React from 'react';
-
-// function ProductList({ products, selectedCategory }) {
-//   // Filter products based on the selected category if it exists
-//   const filteredProducts = selectedCategory ? products.filter(product => product.categoryIds.includes(selectedCategory)) : products;
-
-//   return (
-//     <div className="product-list">
-//       {filteredProducts.map(product => (
-//         <div key={product.id} className="product-item">
-//           <img src={product.image} alt={product.name} className="product-image" />
-//           <p>{product.title}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default ProductList;
-
 import React from 'react';
 
 const ProductList = ({ products }) => {
